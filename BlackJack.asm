dc3fa9727fef8419a906d1c073cf0789131df90a
Edwin Ortega <100718972+EdwinOrtegaK@users.noreply.github.com>
2023-05-25 12:03:14 -0600

    Update BlackJack.asm
    Instrucciones

--- BlackJack.asm
+++ BlackJack.asm
@@ -34,11 +34,22 @@ banner4 BYTE "|B    B  L    A     A C       KK KK      J  J  A     A C      KK K
 banner5 BYTE "|BBBBB   LLL A       A CCCC   KK  KK      JJ  A       A CCCC  KK  KK  |",0Ah,0
 banner6 BYTE "|---------------------------------------------------------------------|",0Ah,0Ah,0
 
+obj BYTE "El objetivo principal del juego es obtener una mano con un valor total más cercano a 21 ",0Ah,0
+acl BYTE  "En el BlackJack, las cartas numéricas son del 1 al 10 (en este juego), por lo que se jugaran con numeros en este intervalo",0Ah, 0
+ins1 BYTE "Se le reparten dos cartas (numeros) para tomar decisiones basadas en sus cartas y poder sumar entre si un valor cercano a 21",0Ah,0
+ins2 BYTE "Después de recibir las dos cartas iniciales, tienes varias opciones:",0Ah,0
+opc1 BYTE "1. Decide no recibir más cartas y quedarte con la puntuación actual",0Ah,0
+opc2 BYTE "2. Solicita una carta adicional para intentar mejorar su puntuación, siempre y cuando no se pase de 21",0Ah,0
+ins3 BYTE "Si tienes una puntuación más cercana a 21 gana la mano y recibe un pago igual a su apuesta ",0Ah,0
+ins4 BYTE "Si se pasa de 21, pierde automáticamente",0Ah,0
+
 play BYTE "1: Jugar",0Ah,0
 rules BYTE "2: Ver instrucciones",0Ah,0
 salir BYTE "3: Terminar de jugar",0Ah,0Ah,0
+grx BYTE "Gracias por jugar",0Ah,0
 
-num dd "%d",0
+num db "%d",0
+num1 dword 0
 
 opcion BYTE "Opcion: ",0
 
@@ -55,6 +66,7 @@ randomNum db 0
 
 public main
 main proc
+	call numRandom
 
 				;Impresión del banner
 	push offset banner0
@@ -93,18 +105,20 @@ Menu:
 	add esp, 4
 	lea eax, [ebp-4]
 
+	lea eax, num1
 	push eax 
 	push offset num
 	call scanf
 
-	add esp, 8
-	mov eax, [ebp-4]
+	mov eax, num1
 
-	add esp, 8
+	cmp eax, 2
+	JE reglas
+
+	cmp eax, 3
+	JE terminar
 
 jugar:
-
-	call numRandom
 
 	push dword ptr [randomNum] ; pasar el valor sum como argumento a printf
 	push offset fmt ; pasar el formato como argumento a printf
@@ -117,7 +131,32 @@ jugar:
 
 reglas:
 
+	;Imprime las instrucciones escritas
+	push offset banner6
+	call printf
+	push offset obj
+	call printf
+	push offset acl
+	call printf
+	push offset ins1
+	call printf
+	push offset ins2
+	call printf
+	push offset opc1
+	call printf
+	push offset opc2
+	call printf
+	push offset ins3
+	call printf
+	push offset ins4
+	call printf
+	push offset banner6
+	call printf
+
+
 terminar:
+	push offset grx
+	call printf
 	push 0
 	call exit ; salir del programa
 
